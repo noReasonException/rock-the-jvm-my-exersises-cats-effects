338ec6c59e8cda4645aa68254a8d15f2f9c8bb35
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-07-12 11:14:09 +0100

    traversable

---
+++ src/main/scala/pt2/TraverseExercises.scala
@@ -0,0 +1,25 @@
+package pt2
+
+import cats.{Parallel, Traverse}
+import cats.effect.{Async, IO, IOApp, Sync}
+import cats.syntax.traverse._
+import cats.syntax.parallel._
+object TraverseExercises extends IOApp.Simple{
+
+  def sequence[A](ios:List[IO[A]]):IO[List[A]] = ios.traverse(identity)
+  def sequence2[C[_]:Traverse,A](ios:C[IO[A]]):IO[C[A]] = ios.traverse(identity)
+  def sequence3[F[_]:Sync,C[_]:Traverse,A](in:C[F[A]]):F[C[A]] = in.traverse(identity)
+
+
+  def parSequence[A](ios:List[IO[A]]):IO[List[A]] = ios.parTraverse(identity)
+  def parSequence2[C[_]:Traverse,A](ios:C[IO[A]]):IO[C[A]] = ios.parTraverse(identity)
+  def parSequence3[F[_]:Parallel,C[_]:Traverse,A](in:C[F[A]]):F[C[A]] = in.parTraverse(identity)
+
+
+
+  override def run: IO[Unit] = IO.unit
+
+
+
+
+}
