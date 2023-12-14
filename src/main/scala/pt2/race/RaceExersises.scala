251758f65f214343c4ccfdfcee4820f3e95c433c
noReasonException <stefannstefanou@gmail.com>
2023-12-14 12:06:12 +0000

    Return the Looser

--- src/main/scala/pt2/race/RaceExersises.scala
+++ src/main/scala/pt2/race/RaceExersises.scala
@@ -1,11 +1,19 @@
 package pt2.race
 
+import cats.effect.kernel.Outcome
 import cats.effect.{IO, IOApp}
-import cats.implicits.catsSyntaxApplicativeId
+import cats.implicits.{catsSyntaxApplicativeId, catsSyntaxEitherId}
 
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 object RaceExersises extends IOApp.Simple {
+
+
+  private def fibToIo[A](outcome:Outcome[IO,Throwable,A]):IO[A] = outcome match {
+    case Outcome.Succeeded(fa) => fa
+    case Outcome.Errored(e) => IO.raiseError(new Throwable(e.toString))
+    case Outcome.Canceled() => IO.raiseError(new Throwable("Cancelled"))
+  }
 
   def timeout[A](ioa:IO[A],duration:FiniteDuration):IO[A] = {
     val timeoutJob = IO.sleep(duration)
@@ -15,5 +23,12 @@ object RaceExersises extends IOApp.Simple {
     }
   }
 
-  override def run: IO[Unit] = timeout(IO.sleep(1.seconds),2.second).void
+  //return the looser
+  def unrace[A,B](ioa:IO[A],iob:IO[B]):IO[Either[A,B]] =
+    ioa.racePair(iob).flatMap {
+      case Left((leftWins,looserFiber)) => looserFiber.join.flatMap(v=>fibToIo(v).map(_.asRight))
+      case Right((looserFiber,rightWins)) => looserFiber.join.flatMap(v=>fibToIo(v).map(_.asLeft))
+    }
+
+  override def run: IO[Unit] = unrace(IO.sleep(2.seconds)>>IO.delay("Left"),IO.sleep(1.seconds)>>IO.delay("Right")).flatMap(IO.println).void
 }
